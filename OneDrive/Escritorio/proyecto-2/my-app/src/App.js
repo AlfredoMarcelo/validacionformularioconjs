80139aebacf3b5b102220abfe248cf564202a7ac
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2021-11-08 20:05:26 -0300

    Avance Seleccion

--- OneDrive/Escritorio/proyecto-2/my-app/src/App.js
+++ OneDrive/Escritorio/proyecto-2/my-app/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Seleccion } from "./components/Seleccion";
+
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo react</h1>
+      <h1>Tic-Tac-Toe in React.js</h1>
+      <Seleccion/>
     </div>
   );
 }
---
+++ OneDrive/Escritorio/proyecto-2/my-app/src/components/Seleccion.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Seleccion = () => {
+    
+    
+    
+    return (
+        <div>
+            <h3>CHOOSE YOUR WEAPON</h3>
+            <form>
+                <input type="text" placeholder="Player 1 username"/>
+                <input type="text" placeholder="Player 2 username"/>
+                <button><h2>X</h2></button>
+                <button><h2>O</h2></button>
+            </form>
+                       
+        </div>
+    )
+}
